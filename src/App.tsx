17bc848cdeacd1b7ec5a10744f301bd89bd689d0
glenadamson <glenadamson1984@gmail.com>
2021-11-22 11:53:26 +0000

    adding styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
 
-const App = function () {
-  return (<div>Example</div>);
-};
+import styled from 'styled-components';
+
+const StyledAppContainer = styled.div`
+  min-height: 100vh;
+  background-color: darkgreen;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = () => (<StyledAppContainer>example</StyledAppContainer>);
 
 export default App;
